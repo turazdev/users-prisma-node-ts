4739b4087925508f37fcd6f70b591f661823bdc1
arturtembe <malunganeartur@gmail.com>
2024-05-13 19:03:12 +0200

    update app  vercel

--- src/server.ts
+++ src/server.ts
@@ -27,3 +27,5 @@ app.use((err:Error,request:Request,response:Response,next: NextFunction)=>{
 app.listen(process.env.PORT || 3333,
     ()=>{console.log("Server is running! in port 3333")}
 )
+
+module.exports = app;
